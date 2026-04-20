e2373114a7c39065509702ad72ca1d642ba91027
hypetrax <hypetrax@gmail.com>
2026-04-20 20:01:29 +0200

    Update header logo text to Landhuis Installatietechniek

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <div className="container header-inner">
           <div className="logo">
             <img src="/logo.webp" alt="Landhuis Logo" className="logo-img" />
-            <span className="logo-text">Landhuis</span>
+            <span className="logo-text">Landhuis Installatietechniek</span>
           </div>
         </div>
       </header>
